b7242918f4feaaa099c850a2d295f6fca6693517
Braulio Bhavamitra <brauliobo@gmail.com>
2019-10-18 18:53:04 -0300

    Save checkpoint

---
+++ reaction/homocysteine -> methionine via methione cycle.rb
@@ -0,0 +1,13 @@
+identifier: homocysteine -> methionine
+via: methionine-cycle
+type: unidirectional
+reagents:
+  - homocysteine
+  - 5-MTHF
+enzymes:
+  - MS
+coenzymes:
+  - B12
+products:
+  - THF
+
